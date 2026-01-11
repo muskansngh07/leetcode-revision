a0521505e775cb4d0bbe9dd97935d9f30b0c4cff
Muskan <muskansngh15@gmail.com>
2026-01-12 00:09:23 +0530

    Determine the length of the last word in the string

---
+++ lastWordLength.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class lastWordLength {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the string: ");
+        String s=sc.nextLine();
+        String[]words=s.trim().split(" ");
+        System.out.println("The length of the last word in the string is "+words[words.length-1].length());
+        sc.close();
+    }
+}
