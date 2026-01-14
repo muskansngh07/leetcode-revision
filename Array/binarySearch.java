fc59614bd674444031cc572d7090e6112e227695
Muskan <muskansngh15@gmail.com>
2026-01-14 15:20:41 +0530

    Search for an element in a sorted array using binary search technique

---
+++ Array/binarySearch.java
@@ -0,0 +1,36 @@
+package Array;
+import java.util.Scanner;
+public class binarySearch {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.print("Enter the size of array: \n");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.print("Enter the element to be searched: ");
+        int target=sc.nextInt();
+        int low=0,high=n-1,count=0;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(target==a[mid])
+            {
+                System.out.println(target+" found at index "+mid);
+                count++;
+                break;
+            }
+            else if(target>a[mid])
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        if(count==0)
+            System.out.println(target+" not found.");
+        sc.close();
+    }
+}
+
